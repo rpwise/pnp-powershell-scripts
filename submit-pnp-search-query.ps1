8bccb12ffc7ceec13e79f69bc05947ac64700343
Rick Wise <rpwise@gmail.com>
2021-03-18 15:58:52 -0400

    adding submit pnpsearchquery example

---
+++ submit-pnp-search-query.ps1
@@ -0,0 +1,14 @@
+# connect to tenant
+connect-pnponline https://tenantname.sharepoint.com
+
+# get content older than a certan date
+$retentionDate = (get-date).AddDays(-4748).ToShortDateString();
+
+# execute search query
+$query = "LastModifiedTimeForRetention<" + $retentionDate;
+
+# execute search query (paginated)
+$results = submit-pnpsearchquery $query;
+
+# execute search query (all)
+#$results = submit-pnpsearchquery $query -all;
